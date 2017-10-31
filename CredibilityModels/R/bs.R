4b14c54e06ca5a3b8ba28ed6ec87178849e8ab12
Andreas <andreas.hausmann1@gmail.com>
2017-10-31 20:44:03 +0100

    erste funktionsdefinition erstellt

---
+++ CredibilityModels/R/bs.R
@@ -0,0 +1,11 @@
+#' Fits Buehlmann-Straub-Model.
+#'
+#' @param formula a formula
+#' @param data a data.frame in which to interpret the variables named in formula.
+#' @param weights expression indicating the column of data containing the weights.
+#' @return bs returns an object of class cm.
+#' @examples
+#' bs(KS ~ Region, testdat, count)
+bs <- function(formula, data, weights) {
+
+}
